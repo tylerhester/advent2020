e34cf430e19138bf29aae2154adb58c7eee2f310
Tyler Hester <tylerhester@utexas.edu>
2020-12-22 17:38:05 -0600

    Completed D2P2. Updated P1 to just keep track of max seat ID as boarding passes are being processed. D5P2 completed by printing out the seating and looking for the missing seat ID.

--- src/day5.cpp
+++ src/day5.cpp
@@ -2,7 +2,6 @@
 
 #include <fstream>
 #include <iostream>
-#include <queue>
 #include <string>
 #include <vector>
 
@@ -11,13 +10,17 @@ using namespace std;
 void findAirplaneSeat(string, int, int, int&);
 void addTicket(int[], const int);
 
+void print2DArray(int[128][8]);
+
 int main(int argc, char* argv[]) {
     ifstream file;
     // file.open("testing5.txt");
     file.open("binarySeating.txt");
 
+    int seats[128][8] = {};
+
     // STL priority_queue functions as max_heap with default parameters
-    priority_queue<int> pq;
+    int maxID = 0, ourID = 0;
 
     string line;
     while (getline(file, line) && !file.eof()) {
@@ -28,14 +31,30 @@ int main(int argc, char* argv[]) {
         findAirplaneSeat(line.substr(line.length() - 3), 0, 7, col);
 
         int seatID = row * 8 + col;
-        pq.push(seatID);
+        seats[row][col] = seatID;
+
+        if (seatID > maxID) maxID = seatID;
         // cout << "Row: " << row << "; Col: " << col << "; ID: " << seatID <<
         // endl;
     }
 
-    cout << "Part 1 > " << pq.top() << endl;
+    cout << "Part 1 > " << maxID << endl;
+
+    // Used to find the missing seat, and it's ID
+    print2DArray(seats);
+
+    cout << "Part 2 > " << ourID << endl;
     file.close();
     return 0;
+}
+
+void print2DArray(int seats[128][8]) {
+    for(int r = 0; r < 128; r++) {
+        for(int c = 0; c < 8; c++) {
+            cout << seats[r][c] << " ";
+        }
+        cout << endl;
+    }
 }
 
 void findAirplaneSeat(string ticket, int beg, int end, int& seat) {
