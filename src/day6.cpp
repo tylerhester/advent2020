dfabee7b606554b4815e145f1bcf9530620217c9
Tyler Hester <tylerhester@utexas.edu>
2020-12-22 19:15:22 -0600

    Day 6, Part 2: Completed. Part Two total is just the number of group with questions that were asked the same amount of times as there are people (assuming each person is not asked the same question twice

--- src/day6.cpp
+++ src/day6.cpp
@@ -1,43 +1,66 @@
-#include <iostream>
-#include <unordered_map>
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     ifstream file;
     // file.open("testing6.txt");
     file.open("customsQuestions.txt");
 
     unordered_map<char, int> questionsAsked;
 
-    int totalAsked = 0;
+    int partOne = 0, partTwo = 0;
+    int totalAskedGroup = 0, totalPeopleGroup = 0;
     string line;
-    while(getline(file, line)) {
-        if(!line.length()) {
+    while (getline(file, line)) {
+        if (!line.length()) {
             // Found next batch of questions
-            cout << questionsAsked.size() << " + ";
-            totalAsked += questionsAsked.size();
+            partOne += totalAskedGroup;
+
+            // Find the total questions that were answered by all
+            int totalAllAns = 0;
+            for (auto kv : questionsAsked) {
+                // cout << kv.first << " " << kv.second << endl;
+                if (kv.second == totalPeopleGroup) totalAllAns++;
+            }
+            partTwo += totalAllAns;
+            // cout << "Total People: " << totalPeopleGroup << "; Part 1 > " << totalAskedGroup << "; Part 2 > " << totalAllAns << endl << endl;
+
+            // New batch of questions for new group
             questionsAsked.clear();
+            totalAskedGroup = 0, totalPeopleGroup = 0;
         } else {
             // Still on current batch of questions
             // Loop through the current line
-            for(auto c : line) {
-                if(questionsAsked[c] == 0) {
+            totalPeopleGroup++;
+            for (auto c : line) {
+                // If question hasn't been asked add it to list
+                if (questionsAsked.find(c) == questionsAsked.end()) {
+                    totalAskedGroup++;
                     questionsAsked.insert(make_pair(c, 1));
+                } else {
+                    questionsAsked[c] = questionsAsked[c] + 1;
                 }
             }
         }
     }
 
-    cout << questionsAsked.size();
-    totalAsked += questionsAsked.size();
-    questionsAsked.clear();
+    partOne += totalAskedGroup;
 
-    cout << " = " << totalAsked << endl;
-    
+    int totalAllAns = 0;
+    for (auto kv : questionsAsked) {
+        // cout << kv.first << " " << kv.second << endl;
+        if (kv.second == totalPeopleGroup) totalAllAns++;
+    }
+    partTwo += totalAllAns;
+    // cout << "Total People: " << totalPeopleGroup << "; Part 1 > " << totalAskedGroup << "; Part 2 > " << totalAllAns << endl << endl;
+
+    cout << "Part 1 Total: > " << partOne << endl;
+    cout << "Part 2 Total: > " << partTwo << endl;
+
     file.close();
     return 0;
 }
